7c12518edf85aa55967aa8d6ec83369c8a92d883
User <User@DESKTOP-AV4MN69>
2021-08-04 11:48:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    /* 
+     ВСЕМ ПРИВЕТ СЕГОДНЯ 19/07/2021 И ЕШИЛ СТАТЬ ПРОГРАММИСТОМ
+    */
+    class Program // СЕГОДНЯ Я УЗНАЛ КАК ДЕЛАЮТСЯ КОМЕНТАРИИ
+    {
+        static void Main(string[] args)
+        {
+            // UNDONE: доработать следующуюся функциональность 
+            Console.Write("Hello World!");
+            // TODO: добавить новую функцию
+            // HACK: this code is temp. it should be fixed or deleted 
+            Console.ReadKey();
+        }
+    }
+}
