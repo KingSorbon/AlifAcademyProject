8e5a420d0bb02b6f4513ed7fa7bf890bd78bd6d7
KingSorbon <88387773+KingSorbon@users.noreply.github.com>
2021-08-18 20:04:18 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ConsoleApp2
 {
@@ -50,18 +51,23 @@ namespace ConsoleApp2
             double resultY2 = Math.Sqrt(y1);
             Console.WriteLine($"Result = {result}");
             Console.ReadKey(); */
-            
-            
-            
-            
+
+
+
+
             //AlifAcademy.HomeWork.03.08.2021
             // ex:1
+            /*Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.White;
+
             double a = 16.80;
             double b= 12.40 ;
-            double result = Math.Sqrt(a*b) ;
+            double result = Math.Sqrt(a * b);
             Console.WriteLine($"Result of 1 EX={result}");
 
             // ex:2
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
             decimal A= 1.40m;
             decimal B= -5.50m;
             decimal C= 0.60m;
@@ -69,12 +75,15 @@ namespace ConsoleApp2
             decimal AC = A - C;
             decimal BC = C - B;
             decimal summa = AC + BC;
-
-            Console.WriteLine("AC=" + AC);
-            Console.WriteLine("BC=" + BC);
+            
             Console.WriteLine("Result of 2 EX:" + summa);
+            Console.WriteLine("Result of AC=" + AC);
+            Console.WriteLine("Result of BC=" + BC);
+           
 
             // ex:3
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
             double x1 = -6.20;
             double y1 = 5.20;
             double x2 = 2.10;
@@ -84,6 +93,8 @@ namespace ConsoleApp2
             Console.WriteLine("Result of 3 EX:" + Distant);
 
             // ex:4
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
             int z = 41;
             int x = z / 10;
             int v = (z%10)*10;
@@ -92,50 +103,243 @@ namespace ConsoleApp2
             Console.WriteLine("Result of 4 EX: " + o);
 
             // ex:5
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.White;
             int N = 10985;
             int R = N / 60;
 
             Console.WriteLine("Result of 5 EX: " + R);
 
             // ex:6
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
             int K = 202;
             int n = K % 7;
 
             Console.WriteLine("Result of 6 EX: " + n);
-            Console.ReadKey();
+            Console.ReadKey();*/
 
             // There is also another way to write this code
 
-            /* if (n == 0)
+            /*if (n == 0)
+           {
+               System.Console.WriteLine("Номер дня недели: 0 - Воскресенье");
+           }
+           else if (n == 1)
+           {
+               System.Console.WriteLine("Номер дня недели: 1 - Понедельник");
+           }
+           else if (n == 2)
+           {
+               System.Console.WriteLine("Номер дня недели: 2 - Вторник");
+           }
+           else if (n == 3)
+           {
+               System.Console.WriteLine("Номер дня недели: 3 - Среда");
+           }
+           else if (n == 4)
+           {
+               System.Console.WriteLine("Номер дня недели: 4 - Четверг");
+           }
+           else if (n == 5)
+           {
+               System.Console.WriteLine("Номер дня недели: 5 - Пятница");
+           }
+           else if (n == 6)
+           {
+               System.Console.WriteLine("Номер дня недели: 6 - Суббота");
+           }*/
+
+            // 05.08.2021 Class Work
+
+            /*int num = int.Parse(Console.ReadLine());
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            string sign = Console.ReadLine();
+            int.TryParse(Console.ReadLine(), out int result);
+            System.Console.WriteLine(result);
+            
+            int n1 = 44;
+            int n2 = 34;
+            int n3 = 5;
+
+            if (n1 > n2)
             {
-                System.Console.WriteLine("Номер дня недели: 0 - Воскресенье");
+                System.Console.WriteLine("n1 > n2");
+                n2 = 0;
             }
-            else if (n == 1)
+            else if (n1 == n2)
             {
-                System.Console.WriteLine("Номер дня недели: 1 - Понедельник");
+                System.Console.WriteLine("n1 == n2");
+                n1 = n3;
             }
-            else if (n == 2)
+            else if (n1 != n2)
             {
-                System.Console.WriteLine("Номер дня недели: 2 - Вторник");
+                System.Console.WriteLine("n1 != n2");
             }
-            else if (n == 3)
+            else
             {
-                System.Console.WriteLine("Номер дня недели: 3 - Среда");
+                System.Console.WriteLine("n1 < n2");
+                n1 = n2 = n3;
             }
-            else if (n == 4)
+            switch (n1)
             {
-                System.Console.WriteLine("Номер дня недели: 4 - Четверг");
+                case 0:
+                    {
+                        System.Console.WriteLine("zero");
+                    }
+                    break;
+                case 1:
+                    {
+                        System.Console.WriteLine("One");
+                    }
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    {
+                        System.Console.WriteLine("from two to four");
+                    }
+                    break;
+                default:
+                    {
+                        System
+                        .Console
+                        .WriteLine("There is no number from 0 to 10");
+                    }
+                    break;
             }
-            else if (n == 5)
+            System.Console.WriteLine(n2);
+            int number = 7;
+            // bool isPositive = nu
+
+
+            int A = 15;
+            int B = 15;
+            if (A > B)
             {
-                System.Console.WriteLine("Номер дня недели: 5 - Пятница");
+                System.Console.WriteLine("");
+
             }
-            else if (n == 6)
+            else if (A == B)
             {
-                System.Console.WriteLine("Номер дня недели: 6 - Суббота");
+                System.Console.WriteLine("0");
             }*/
 
+
+            //HomeWork 05.08.2021
+            //задания 2
+            int A = 10;
+            int B = 11;
+            if (A > B)
+            {
+                A = B;
+            }
+            else if (B > A)
+            {
+                A = B;
+            }
+            else
+            {
+                A = B = 0;
+            }
+            Console.WriteLine($"A = {A} B = {B}");
+            Console.ReadKey();
+
+            // задания 3
+            Console.Write("Enter operand1 : ");
+            double operand1 = Double.Parse(Console.ReadLine());
+            Console.Write("Enter operand2 : ");
+            double operand2 = Double.Parse(Console.ReadLine());
+
+            Console.Write("Enter operator + - * / :  ");
+            var sign = Console.ReadLine();
+
+            switch (sign)
+            {
+                case ("+"):
+                    System.Console.WriteLine(operand1 + operand2);
+                    break;
+                case ("-"):
+                    System.Console.WriteLine(operand1 - operand2);
+                    break;
+                case ("*"):
+                    System.Console.WriteLine(operand1 * operand2);
+                    break;
+                default:
+                    try
+                    {
+                        System.Console.WriteLine(operand1 / operand2);
+                    }
+                    catch (System.DivideByZeroException)
+                    {
+                        System.Console.WriteLine("Your Exception notification");
+                    }
+                    break;
+            }
+            Console.ReadKey();
+
+            //задания 4
+            Console.WriteLine("Enter any number");
+            var num = int.Parse(Console.ReadLine());
+            if (num >= 0 && num <= 14)
+            {
+                Console.WriteLine("Number is between 0 and 14");
+            }
+            else if (num >= 15 && num <= 35)
+            {
+                Console.WriteLine("Number is between 15 and 35");
+            }
+            else if (num >= 36 && num < 50)
+            {
+                Console.WriteLine("Number is between 36 and 50");
+            }
+            else if (num >= 50 && num <= 100)
+            {
+                Console.WriteLine("Number is between 50 and 100");
+            }
+            else if (num >= 100)
+            {
+                Console.WriteLine("I said any number but between 0 and 100 ");
+            }
+            Console.ReadKey();
+
+
+            //homework 07.08.2021
+            //Задания 1
+            /*System.Console.Write("Введите сумму покупки: ");
+            var X = decimal.TryParse(Console.ReadLine(), out var x);
+            
+            if (!X)
+            {
+                System.Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.WriteLine("Вы ввели не число");
+                System.Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (x < 0)
+            {
+                System.Console.WriteLine("Сумма покупки не может быть меньше 0");
+            }
+            else if (x < 500)
+            {
+                System.Console.WriteLine($"Сумма к оплате: {x} самон");
+            }
+            else if (x > 1000)
+            {
+                var v = x / 100 * 5;
+                var y = x - v;
+                System.Console.WriteLine($"Ваша скидка 5% составляет: {v} самон");
+                System.Console.WriteLine($"Сумма к оплате: {y} самон");
+            }
+            else if (x > 500)
+            {
+                var v = x / 100 * 3;
+                var y = x - v;
+                System.Console.WriteLine($"Ваша скидка 3% составляет: {v} самон");
+                System.Console.WriteLine($"Сумма к оплате: {y} самон");
+            }*/
+
+            // задания 2
+
         }
-       
     }
 }
